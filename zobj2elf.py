3eaee15bfbd882f99afa3d091daa7311a79c2603
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-08-07 22:45:25 +0700

    Use `argparse`

--- zobj2elf.py
+++ zobj2elf.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser, Namespace
 
 
-def main() -> int:
+def main(args: Namespace) -> int:
     print("Stub")
     return 0
 
 
 if __name__ == "__main__":
-    exit(main())
+    parser = ArgumentParser(prog="zobj2elf", description="Convert Z80ASM object files to ELF")
+
+    parser.add_argument("input", help="Input file")
+    parser.add_argument("output", help="Output file")
+
+    exit(main(parser.parse_args()))
