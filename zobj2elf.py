e929c53a0b24acd79dcc445fab8329aa0b0d3f06
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-08-08 20:29:49 +0700

    Initial ELF creation

--- zobj2elf.py
+++ zobj2elf.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser, Namespace
 
+from makeelf.elf import ELF
+from makeelf.elfstruct import ET
+
 from obj import ZObj
 
 
 def main(args: Namespace) -> int:
     obj = ZObj(open(args.input, "rb"))
+    elf = ELF(e_type=ET.ET_REL)  # makeelf does not support Z80 machine type
+
+    if args.output is None:
+        args.output = args.input[:args.input.rfind(".")] + ".elf"
+    
+    with open(args.output, "wb") as f:
+        f.write(bytes(elf))
+
     return 0
 
 
