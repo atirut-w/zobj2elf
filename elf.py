7d5a97f560300dd25e5f675a08485c6e8427302a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-08-10 15:14:55 +0700

    Add ELF type enum

--- elf.py
+++ elf.py
@@ -15,6 +15,14 @@ class ABI(Enum):
     LINUX = 0x03
 
 
+class Type(Enum):
+    NONE = 0x00
+    REL = 0x01
+    EXEC = 0x02
+    DYN = 0x03
+    CORE = 0x04
+
+
 class ISA(Enum):
     NONE = 0x00
     X86 = 0x03
