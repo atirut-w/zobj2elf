f09dad859246b1048329df75187b28d5889afc5b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-08-10 13:51:57 +0700

    Bitness and Endianness enum

--- elf.py
+++ elf.py
@@ -1,6 +1,16 @@
 from enum import Enum
 
 
+class Bitness(Enum):
+    BIT32 = 0x01
+    BIT64 = 0x02
+
+
+class Endianness(Enum):
+    LITTLE = 0x01
+    BIG = 0x02
+
+
 class ABI(Enum):
     SYSTEM_V = 0x00
     HP_UX = 0x01
