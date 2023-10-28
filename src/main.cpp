3b28aca6aa6400c4b860de06b9e66044b3be3d8a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-10-28 20:24:06 +0700

    Implement arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <argparse/argparse.hpp>
+#include <string>
+#include <filesystem>
 
 using namespace std;
+using namespace argparse;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    ArgumentParser program("zobj2elf");
+    program.add_argument("input")
+        .help("input file");
+    
+    program.add_argument("-o", "--output")
+        .help("output file")
+        .default_value(string("a.out"));
+
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
+
+    if (!filesystem::exists(program.get<string>("input")))
+    {
+        cerr << "Input file `" << program.get<string>("input") << "` does not exist." << endl;
+        exit(1);
+    }
+
     return 0;
 }
